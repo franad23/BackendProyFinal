79ac0efc1f245dd32dedf1723626f18b44041289
Franco Andres Diaz <126727423+franad23@users.noreply.github.com>
2023-08-16 22:13:47 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://echosurvey.netlify.app/', // Cambia esto por la URL de tu frontend
+  origin: 'https://echosurvey.netlify.app', // Cambia esto por la URL de tu frontend
   credentials: true // Habilita el uso de credenciales (cookies)
 }));
 
