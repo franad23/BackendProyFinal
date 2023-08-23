41ab7c45e574623716d463dd1ce2c8affd8eb0ce
Franco Andres Diaz <126727423+franad23@users.noreply.github.com>
2023-08-23 14:27:59 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', // Cambia esto por la URL de tu frontend
+  origin: 'https://frontend-proy-final-igrg.vercel.app', // Cambia esto por la URL de tu frontend
   credentials: true // Habilita el uso de credenciales (cookies)
 }));
 
